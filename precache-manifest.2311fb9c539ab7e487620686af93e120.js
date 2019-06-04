4c41c8a1f0126d3686fb1a72a3fb28129ac64ae7
weiwei jin <weiweijin@codingmole.attlocal.net>
2019-06-04 14:01:52 -0400

    Updates

--- precache-manifest.2311fb9c539ab7e487620686af93e120.js
+++ precache-manifest.2311fb9c539ab7e487620686af93e120.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9b4dd48fe392cf5a914e386ee3687130",
+    "revision": "ced38c46ef31ab6829a8d643824fa39e",
     "url": "/travel/index.html"
   },
   {
-    "revision": "0e27b7cea3c0bb3441e7",
+    "revision": "2a9afa9614a26fe1cd7a",
     "url": "/travel/static/css/main.2cce8147.chunk.css"
   },
   {
-    "revision": "2e0129063922e3bb197a",
-    "url": "/travel/static/js/2.b41502e9.chunk.js"
+    "revision": "2315f74ce3c99316133d",
+    "url": "/travel/static/js/2.3310f33a.chunk.js"
   },
   {
-    "revision": "0e27b7cea3c0bb3441e7",
-    "url": "/travel/static/js/main.5e8af432.chunk.js"
+    "revision": "2a9afa9614a26fe1cd7a",
+    "url": "/travel/static/js/main.62c68e0a.chunk.js"
   },
   {
     "revision": "863f8cee14e2ca72744b",
